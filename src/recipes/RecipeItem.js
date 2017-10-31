8f6b97bd728d3290638e76391cc9ff5051fa8d1c
Wouter de Vos <wouter@codaisseur.com>
2017-10-31 18:21:39 +0100

    Some styling and a like button

--- src/recipes/RecipeItem.js
+++ src/recipes/RecipeItem.js
@@ -1,9 +1,13 @@
 // src/recipes/RecipeItem.js
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
+import LikeButton from '../components/LikeButton'
 import Vegan from '../images/vegan.svg'
 import Vegetarian from '../images/vegetarian.svg'
 import Pescatarian from '../images/pescatarian.svg'
+import './RecipeItem.css'
+
+const PLACEHOLDER = 'http://via.placeholder.com/500x180?text=No%20Image'
 
 class RecipeItem extends PureComponent {
   static propTypes = {
@@ -19,8 +23,16 @@ class RecipeItem extends PureComponent {
 
     return(
       <article className="RecipeItem">
-        <h1>{ title }</h1>
-        <img src={photo} alt={title} />
+        <header>
+          <div
+            className="cover"
+            style={{ backgroundImage: `url(${photo || PLACEHOLDER })` }} />
+
+          <h1>
+            { title }
+          </h1>
+        </header>
+
         <div>
           <p>{ summary }</p>
           <ul>
@@ -28,6 +40,7 @@ class RecipeItem extends PureComponent {
             { !vegan && vegetarian && <li><img src={Vegetarian} alt="vegetarian" /></li> }
             { pescatarian && <li><img src={Pescatarian} alt="pescatarian" /></li> }
           </ul>
+          <LikeButton />
         </div>
       </article>
     )
