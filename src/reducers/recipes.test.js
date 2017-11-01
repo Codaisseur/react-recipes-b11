70e630f50a37c9b1a58245dd274aa57707233e86
Wouter de Vos <wouter@codaisseur.com>
2017-11-01 14:28:10 +0100

    Test recipes reducer

--- src/reducers/recipes.test.js
+++ src/reducers/recipes.test.js
@@ -2,6 +2,7 @@
 
 import { expect } from 'chai'
 import recipes from './recipes'
+import { SEED_RECIPES } from '../actions/recipes/seed'
 
 describe('recipes reducer', () => {
   const reducer = recipes
@@ -9,5 +10,28 @@ describe('recipes reducer', () => {
 
   it('returns an empty array for the initial state', () => {
     expect(reducer()).to.eql(initialState)
+  })
+
+  it(SEED_RECIPES, () => {
+    const eventualState = ['any', 'array', 'will', 'do']
+
+    const seedAction = {
+      type: SEED_RECIPES,
+      payload: eventualState
+    }
+
+    expect(reducer(initialState, seedAction)).to.eql(eventualState)
+  })
+
+  it('NOT_A_VALID_ACTION_TYPE', () => {
+    const initialState = ['any', 'array', 'will', 'do']
+    const eventualState = [ ...initialState ]
+
+    const nonValidAction = {
+      type: 'NOT_A_VALID_ACTION_TYPE',
+      payload: { foo: 'bar' }
+    }
+
+    expect(reducer(initialState, nonValidAction)).to.eql(eventualState)
   })
 })
